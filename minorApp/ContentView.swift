64641c7dcbdeb86207b88118de0126fedf652619
Sean <sean@fiv4lab.com>
2021-07-31 16:15:58 -0400

    Initial Commit

---
+++ minorApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  minorApp
+//
+//  Created by SEAN BLAKE on 7/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
